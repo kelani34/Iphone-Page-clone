d7069095545fcf3fb3a4443f91c1a67bfdcfd8b2
kelani34 <kelanitaiwo34@gmail.com>
2023-06-17 12:32:44 +0100

    update: quotes styling

--- src/sections/Quote.js
+++ src/sections/Quote.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
 const Quote = () => {
-  return <div>Quote</div>;
+  return (
+    <QuotesSection>
+      <div>Line 1</div>
+      <div>Line 2</div>
+      <div>Line 3</div>
+      <div>Line 4</div>
+      <div>Line 5</div>
+    </QuotesSection>
+  );
 };
 
 export default Quote;
+
+const QuotesSection = styled.section`
+  width: 100vh;
+  height: 100vh;
+  position: relative;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: var(--dark);
+  color: var(--white);
+`;
