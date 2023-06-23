d0f83810b73eb297ad8128b9371d18551e63052f
kelani34 <kelanitaiwo34@gmail.com>
2023-06-23 02:07:48 +0100

    update: battery section update

--- src/sections/Battery.js
+++ src/sections/Battery.js
@@ -1,11 +1,85 @@
-import React from "react";
+import gsap from "gsap";
+import React, { useLayoutEffect, useRef } from "react";
 import styled from "styled-components";
 
 const Battery = () => {
-  return <div>Battery</div>;
+  const battery = useRef(null);
+
+  let elements = gsap.utils.selector(battery);
+
+  useLayoutEffect(() => {
+    console.log(elements("li"));
+    let t1 = gsap.timeline({});
+
+    elements("li").forEach((element) => {
+      t1.to(element, {
+        scrollTrigger: {
+          trigger: element,
+          start: "top center",
+          end: "bottom center",
+          scrub: true,
+          markers: true,
+        },
+        opacity: 1,
+      });
+    });
+  }, []);
+  return (
+    <Section>
+      <Title>Go all day with single charge...</Title>
+      <BatterySection ref={battery}>
+        <li />
+        <li />
+        <li />
+        <li />
+        <li />
+      </BatterySection>
+    </Section>
+  );
 };
 
 export default Battery;
 
 const Section = styled.section`
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  background-color: var(--white);
+`;
+
+const Title = styled.h1`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-55%, -50%);
+
+  text-transform: uppercase;
+  font-size: var(--fontBig);
+  z-index: 1;
+`;
+
+const BatterySection = styled.ul`
+  position: absolute;
+  right: 4rem;
+  list-style: none;
+  background-color: var(--white);
+  border: 3px solid var(--dark);
+  border-radius: 8px;
+  padding: 0.5rem;
+  width: 15rem;
+
+  li {
+    width: 100%;
+    height: 5rem;
+    background-color: var(--dark);
+    background-image: linear-gradient(-90deg, var(--gradient));
+    opacity: 0;
+  }
+  & > *:not(:first-child):not(:last-child) {
+    margin: 0.5rem 0;
+  }
 `;
